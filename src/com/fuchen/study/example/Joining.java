1c5c0c9e8530e8873196b246bc9cc733a6eeaa5e
qiaojiangsong@gmail.com <Github3010>
2019-05-01 21:40:03 +0800

    加入一个线程（join）

---
+++ src/com/fuchen/study/example/Joining.java
@@ -0,0 +1,58 @@
+package com.fuchen.study.example;
+
+/**
+ * @Description 加入一个线程（join）
+ * @Author fuchen
+ * @Date 5/1/2019 9:29 PM
+ * Version 1.0
+ */
+public class Joining {
+    public static void main(String[] args) {
+        Sleeper
+                sleepy = new Sleeper("Sleepy", 1500),
+                grumpy = new Sleeper("Grumpy", 1500);
+        Joiner
+                dopey = new Joiner("Dopey", sleepy),
+                doc = new Joiner("Doc", grumpy);
+        grumpy.interrupt();
+    }
+}
+
+class Sleeper extends Thread {
+    private int duration;
+    public Sleeper(String name, int sleepTime) {
+        super(name);
+        duration = sleepTime;
+        start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            sleep(duration);
+        } catch (InterruptedException e) {
+            System.out.println(getName() + " was idInterrupted() " + isInterrupted());
+            return;
+        }
+        System.out.println(getName() + " has awakend ");
+    }
+}
+
+class Joiner extends Thread {
+    private Sleeper sleeper;
+    public Joiner(String name, Sleeper sleeper) {
+        super(name);
+        this.sleeper = sleeper;
+        start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            sleeper.join();
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println(getName() + " join completed ");
+    }
+}
